6dcfe6d1a18af83dd78086836b6c4b6405e91706
Ihme11 <mihail.novikow0@gmail.com>
2022-03-28 17:12:06 +0300

    Fixed typo in user_states

--- user_states.py
+++ user_states.py
@@ -1,7 +1,7 @@
 from collections import defaultdict
 
 # Three states - to start, then add address then add comment
-START, ADD_ADDRESS, ADD_COMMENT = range(2)
+START, ADD_ADDRESS, ADD_COMMENT = range(3)
 USER_STATE = defaultdict(lambda x: START)
 # Temporary save the address in this variable to pass from _add_address to _add_comment func
 ADDRESS = ''
