301577f92c09fce9f3697cb9c9012dc08cb76071
Ihme123 <34493667+Ihme123@users.noreply.github.com>
2022-04-30 14:52:10 +0300

    Delete config.py

--- config.py
+++
@@ -1,3 +0,0 @@
-BOT_TOKEN = '5165810981:AAGFBfjciur8b_jx_QUTGkrSWjafD2NL2kc'
-APP_URL = 'https://my-telebot-ihme.herokuapp.com/' + BOT_TOKEN
-DB_URI = 'postgres://nlsdaolaxcrjrc:ec45371b748dd976996bfe43c2e8eaaec91f86a3b069e30dcc78feb5b1b191c4@ec2-52-212-228-71.eu-west-1.compute.amazonaws.com:5432/d7dnb1t7p4r23c'
