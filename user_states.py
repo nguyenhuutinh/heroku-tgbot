16ca9064bba4aca124c9d50d0a99f2bceca97a0a
T Nguyen <nguyenhuutinh@users.noreply.github.com>
2022-08-23 02:58:45 +0700

    Update user_states.py

--- user_states.py
+++ user_states.py
@@ -2,14 +2,14 @@ from collections import defaultdict
 
 # Three states - to start, then add address then add comment
 START, ADD_ADDRESS, ADD_COMMENT = range(3)
-USER_STATE = defaultdict(lambda x: START)
+USER_STATE = defaultdict(lambda : START)
 # Temporary save the address in this variable to pass from _add_address to _add_comment func
 ADDRESS = ''
 
 
 def get_state(message):
-    return USER_STATE[x = message.chat.id]
+    return USER_STATE[message.chat.id]
 
 
 def update_state(message, state):
-    USER_STATE[x = message.chat.id] = state
+    USER_STATE[message.chat.id] = state
