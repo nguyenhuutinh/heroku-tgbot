b630f415f5a6fc41a7aa69858303e8c0205fc079
T Nguyen <nguyenhuutinh@users.noreply.github.com>
2022-08-23 01:51:23 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import psycopg2
 from flask import Flask, request
 import user_states
 
-db_connection = psycopg2.connect("   postgres://imufmulnjjiqnt:829b20d89a1c04269a2acce443e9171042f4e40455fe99496be08fe54d2a2fee@ec2-54-160-109-68.compute-1.amazonaws.com:5432/dbp2nedqcukgic", sslmode="require")
+db_connection = psycopg2.connect("postgres://imufmulnjjiqnt:829b20d89a1c04269a2acce443e9171042f4e40455fe99496be08fe54d2a2fee@ec2-54-160-109-68.compute-1.amazonaws.com:5432/dbp2nedqcukgic", sslmode="require")
 db_object = db_connection.cursor()
 
 BOT_TOKEN = "5697634365:AAEUgF96qD1wcXtxF5x1tXJSH5CjU18KAYM"
