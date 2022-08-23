d622f76baa31224dc287907fa32d73f680ac7509
Jack N <nguyenhuutinh88@gmail.com>
2022-08-23 16:21:25 +0700

    Update main.py

--- main.py
+++ main.py
@@ -134,13 +134,13 @@ def foo(message):
     lastName = message.from_user.last_name
     username = message.from_user.username
     if "TCCL Community".lower() in firstName.lower() or (lastName != None and "TCCL Community".lower() in lastName.lower()):
-        bot.reply_to(message, ":exclamation: Dùng Tên Nick phạm quy :exclamation:")
+        bot.reply_to(message, ":bangbang: Dùng Tên Nick phạm quy :bangbang:")
         return
     if "TCCL".lower()  in firstName.lower()  or (lastName != None and  "TCCL".lower()  in lastName.lower()  ):
-        bot.reply_to(message, ":exclamation: Dùng Tên Nick phạm quy :exclamation:")
+        bot.reply_to(message, ":bangbang: Dùng Tên Nick phạm quy :bangbang:")
         return
     if username != None and "tccl" in username :
-        bot.reply_to(message, ":exclamation: Dùng Tên Nick phạm quy :exclamation:")
+        bot.reply_to(message, ":bangbang: Dùng Tên Nick phạm quy :bangbang:")
         return
 
 # @bot.message_handler(content_types=[
